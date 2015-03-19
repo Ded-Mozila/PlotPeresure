a9028a21d85468927b6306ebeee4d2f7b010cf41
Ded-Mozila <zyryanov.aleksander@outlook.com>
2015-03-19 18:25:27 +1000

    Реализация функций сделана на 50% отсалось отладитьи пределать все во float

--- py_plot.py
+++ py_plot.py
@@ -1,4 +1,8 @@
 # from numpy import reshape
+
+from pylab import *
+import numpy as np
+
 # from pylab import *
 def openFile(str):
 	fd = open(str,'r')
@@ -10,11 +14,19 @@ def openFile(str):
 	mat = {}
 	j = 0
 	for i in table_lines:
-		mat[j] = i.split("    ")
+		mat[j] = float(i.split("    "))
 		j = j + 1
 	return mat	
 def main():
 	lat = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lat.grd")
 	lon = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lon.grd")
+	presure = openFile("/home/aleks/Aleksander/study/Credential/Project/ReceiveInformation_1/FILE/presure_0.txt")
+	plt.figure()
+	# X , Y = np.meshgrid(lon, lat)
+	CS = plt.contour(lat,lon,presure)
+	plt.clabel(CS, inline=1, fontsize=10)
+	plt.title('Simplest default with labels')
+	# clabel(cset,fmt="%1.1f",fontsize=9)
+	#savefig('pylab.eps')
 	return 0
 main()
