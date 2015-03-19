f52477a0414b49b9d73189fe77e6643e6baed4c1
Ded-Mozila <zyryanov.aleksander@outlook.com>
2015-03-19 23:14:15 +1000

    Настроена отрисовка давления без точных координат

--- py_plot.py
+++ py_plot.py
@@ -1,32 +1,29 @@
-# from numpy import reshape
 
 from pylab import *
-import numpy as np
-
-# from pylab import *
+import re
 def openFile(str):
 	fd = open(str,'r')
-	s = []
 	for i in range(5):
-		s += fd.readline()
-	table_lines = fd.readlines()
+		s = fd.readline()
+	mat = []
+	for i in fd:
+		mt =[float(field) for field in i.strip().lstrip('[').rstrip(']').split()]
+		mat.append(mt)
 	fd.close()
-	mat = {}
-	j = 0
-	for i in table_lines:
-		mat[j] = float(i.split("    "))
-		j = j + 1
 	return mat	
 def main():
 	lat = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lat.grd")
 	lon = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lon.grd")
 	presure = openFile("/home/aleks/Aleksander/study/Credential/Project/ReceiveInformation_1/FILE/presure_0.txt")
 	plt.figure()
-	# X , Y = np.meshgrid(lon, lat)
-	CS = plt.contour(lat,lon,presure)
+	CS = plt.contour(presure,antialiased = True)
 	plt.clabel(CS, inline=1, fontsize=10)
 	plt.title('Simplest default with labels')
-	# clabel(cset,fmt="%1.1f",fontsize=9)
-	#savefig('pylab.eps')
+
+	plt.figure()
+	CS = plt.contour(presure)
+	plt.clabel(CS, inline=1, fontsize=10)
+	plt.title('Simplest default with labels')
+	plt.show()
 	return 0
 main()
