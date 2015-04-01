5d39c16bac3fd3808ead13c03e40a9ddb8f1253d
Ded-Mozila <zyryanov.aleksander@outlook.com>
2015-04-01 11:03:21 +1000

    Небольшие изменение : Добавил функцию для отрисовки одной картинки и добавил дополнительную настройку отрисовки

--- py_plot.py
+++ py_plot.py
@@ -1,7 +1,9 @@
 
 from pylab import *
-import re
-def openFile(str):
+# import matplotlib.image as mpimg
+import os
+def openF
+ile(str):
 	fd = open(str,'r')
 	for i in range(5):
 		s = fd.readline()
@@ -10,20 +12,44 @@ def openFile(str):
 		mt =[float(field) for field in i.strip().lstrip('[').rstrip(']').split()]
 		mat.append(mt)
 	fd.close()
-	return mat	
+	return mat
+def dirList(str):
+	lt =  os.listdir(str)
+	return lt
+# name
 def main():
-	lat = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lat.grd")
-	lon = openFile("/home/aleks/Aleksander/study/Credential/sasha_diplom_data/2013.12.11/lon.grd")
-	presure = openFile("/home/aleks/Aleksander/study/Credential/Project/ReceiveInformation_1/FILE/presure_0.txt")
-	plt.figure()
-	CS = plt.contour(presure,antialiased = True)
-	plt.clabel(CS, inline=1, fontsize=10)
-	plt.title('Simplest default with labels')
+	fs = open("settings.txt")
+	str = fs.readline()
+	fs.close()
+	lt = dirList(str)
+	for name in lt:
+		print name
+		os.makedirs(name+"/gif/",0755 )
+		# lat = openFile(str+name+"/lat.grd")
+		# lon = openFile(str+name+"/lon.grd")
+		file_dir = dirList(str+name)
+		for x in file_dir:
+			if x.find("presure") != -1:
+				print x
+				presure = openFile(str+name+"/"+x)
+				plt.figure()
+				# img = mpimg.imread('d01_khv75_blank.png')
+				# plt.imshow(img)
+				CS = plt.contour(presure)
+				plt.clabel(CS, inline=1, fontsize=10)
+				plt.title('Simplest default with ' + x)
+				plt.savefig(name+'/gif/'+ x + '.png')
+	return 0
 
+
+def one_file(str):
+	presure = openFile(str)
 	plt.figure()
 	CS = plt.contour(presure)
 	plt.clabel(CS, inline=1, fontsize=10)
-	plt.title('Simplest default with labels')
+	#plt.title('Simplest default with ' + x)
+	#plt.savefig(name+'/gif/'+ x + '.png')
 	plt.show()
 	return 0
-main()
+one_file("/home/aleks/Aleksander/study/Credential/Project/ReceiveInformation_1/FILE/2013.12.11/test2.txt")
+#main()
